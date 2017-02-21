a17d87109d8a2b9388a970d5d98d8fcf09ffb7fa
Raul Leal <raleba91@gmail.com>
2017-02-21 20:51:55 +0100

    Add files via upload
    Cambio en los sockets para comunicación via puerto 80 y 443 para que realice las peticiones de forma correcta teniendo en cuenta la cabecera Location, debido a que si no se tiene en cuenta, puede mostrar una cabecera incorrecta. 
    Añadido también el Host al enviar el mensaje, ya que algunos servidores web, pueden mostrar un error 404 si no se incluye esta cabecera en el mensaje.
    Añadido tratamiento para conexiones a mongodb puerto 27017 para obtener el banner del servidor y caracteristicas de su contenido

--- nobita-bot.py
+++ nobita-bot.py
@@ -23,22 +23,24 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #***********BOT PRINCIPAL******************##
 #BOT NAME: NOBITA                          ##
-#BOT ENCARGADO DE OBTENER EL               ## 
+#BOT ENCARGADO DE OBTENER EL               ##
 #BANNER_GRABBING DE LAS IPS GENERADAS      ##
 #POR EL BOT                                ##
 #############################################
 
-import socket, urllib, sys, os, time, json
+import socket, urllib, sys, os, time, json, ssl
 import GeoIP
 import netaddr
 from pymongo import MongoClient
+from urlparse import urlparse
 
 # Configuracion
-# portList = [21,22,23,25,53,63,80,90,110,143,161,443,500,513,520,559,3306,3389,5000,5050, 5060, 8069,8080, 9443,27017, 28017] 
-portList = [80, 443]
+#portList = [21,22,23,25,53,63,80,90,110,143,161,443,500,513,520,559,3306,3389,5000,5050, 5060, 8069,8080, 9443,27017, 28017]
+portList = [80, 443, 8080, 27017]
 totalPuertos = len(portList)
 ip_root = ""
 timeout = 3
+_lastlocation = ''
 
 
 def insert_mongodb(IP, Country, City, regionName, ISP, Port, Banner, Latitud, Longitud, date_Insert, date_Update):
@@ -50,51 +52,105 @@ def insert_mongodb(IP, Country, City, regionName, ISP, Port, Banner, Latitud, Lo
     except:
         print "[WARNING]ERROR INSERT MONGODB"
 
-
 def geoIp(IP):
-    # return urllib.urlopen("http://ip-api.com/json/" + str(IP))
+
+    #return urllib.urlopen("http://ip-api.com/json/" + str(IP))
     gi = GeoIP.open("/usr/share/GeoIP/GeoIPCity.dat", GeoIP.GEOIP_STANDARD)
     gir = gi.record_by_addr(IP)
     print gir
     return gir
 
-
 '''Grab Banner'''
-def banner_grabbing_web(ip_address,port):  
-    global portList
-    try:  
-        s=socket.socket()  
-        s.settimeout(timeout)
-        s.connect((ip_address,port))
-        s.send("GET HTTP/1.1 \r\n")
-        banner = s.recv(2048)  
-        print "[+]" + ip_address + ' : ' + str(port) + ' -BANNER- ' + banner + time.strftime("%H:%M:%S") + ' '
+def banner_grabbing_web(ip_address, port, path='/'):
+    global _lastlocation
+
+    try:
+        banner = str()
+        location = None
+
+        if port in [80, 8080, 28017]:
+            s = socket.socket()
+            s.settimeout(timeout)
+        elif port == 443:
+            socketssl = socket.socket()
+            socketssl.settimeout(timeout)
+            s = ssl.wrap_socket(socketssl)
+        s.connect((ip_address, port))
+        s.send("GET "+path+" HTTP/1.1\r\nHost: "+ip_address+"\r\n\r\n")
+
+        message = s.recv(2048)
+
+        for bannerline in message.split('\r\n'):
+            if len(bannerline) != 0:
+                banner += bannerline+'\n'
+                if 'location' in bannerline.lower():
+                    location = bannerline.lower()
+            else:
+                break
+
+        if location:
+            _getlocation = location[location.index(':')+1:].strip()
+            _newurl = urlparse(_getlocation)
+            _lenurlbase = len(_newurl.scheme + '://' + _newurl.netloc)
+            _newpath = _getlocation[_lenurlbase:]
+
+            if _lastlocation != _newpath:
+                print 1
+                _lastlocation = _newpath
+                banner = banner_grabbing_web(_newurl.netloc, port, _newpath)
         return banner
-    except:  
+
+    except:
         return "none"
 
+def banner_grabbing_mongo(ip_address, port):
 
-def banner_grabbing(ip_address,port):  
+    try:
+        mongo = MongoClient(ip_address, port)
+        infoserver = mongo.server_info()
+        databasesname = mongo.database_names()
+        serverstatus = ''
+        listdatabases = ''
+        banner = "none"
+        for namedb in databasesname:
+
+            try:
+                if serverstatus != mongo[namedb].command('serverStatus'):
+                    serverstatus = mongo[namedb].command('serverStatus')
+            except:
+                pass
+            try:
+                listdatabases = mongo[namedb].command({'listDatabases': 1})
+            except Exception as error:
+                if 'listDatabases may only be run against the admin database.' in error:
+                    databasesname.append('admin')
+
+        if len(infoserver) != 0 or len(serverstatus) != 0 or len(listdatabases) != 0:
+            banner = str(infoserver)+'\n'+str(serverstatus)+'\n'+str(listdatabases)
+
+        return banner
+    except:
+        return "none"
+
+def banner_grabbing(ip_address, port):
     global portList
-    try:  
-        s=socket.socket()  
+    try:
+        s=socket.socket()
         s.settimeout(5.0)
-        s.connect((ip_address,port))  
-        banner = s.recv(2048)  
+        s.connect((ip_address, port))
+        banner = s.recv(4096)
         pct = str(porcentaje(portList.index(port)))
         print "[+]" + ip_address + ' : ' + str(port) + ' -BANNER- ' + banner \
                 + time.strftime("%H:%M:%S") + ' ' + pct + '%'
         return banner
-    except: 
+    except:
         return "none"
-
 
 def porcentaje(portID):
     global portList, totalPuertos
     total = (portID + 1) * 100
     total = total / totalPuertos
     return total
-    
 
 def is_valid_cidr(s):
     try:
@@ -102,9 +158,8 @@ def is_valid_cidr(s):
         return True
     except:
         return False
-        
 
-def main():  
+def main():
     global portList, totalPuertos, ip_address
     f = open("dic/targets.txt", "r")
     targets = f.readlines()
@@ -119,16 +174,23 @@ def main():
                 print "----------------------------------------"
                 print "[INFO] Connecting to: " + str(ip_address)
                 for port in portList:
+                    global _lastlocation
+                    _lastlocation = ''
                     pct = str(porcentaje(portList.index(port)))
                     print "|----[!] " + str(ip_address) + " -> " + str(port) + " " + pct + "%"
-                    # Obtenemos el mensaje del servidor en el puerto 
-                    if port == 80 or port == 8080 or 27017 or 28017:
+                    # Obtenemos el mensaje del servidor en el puerto
+                    webport = [80, 8080, 443, 28017]
+                    if port in webport:
                         Banner = banner_grabbing_web(ip_address, port)
+                    elif port == 27017:
+                        Banner = banner_grabbing_mongo(ip_address, port)
                     else:
-                        Banner = banner_grabbing(ip_address,port)
+                        Banner = banner_grabbing(ip_address, port)
+
                     if Banner == "none":
                         pass
                     else:
+                        print "[+]" + ip_address + ' : ' + str(port) + ' -BANNER- \n' + Banner + time.strftime("%H:%M:%S") + ' '
                         # Variables obtenidas de la geoIp
                         data_geoIP = geoIp(ip_address)
                         if data_geoIP:
@@ -140,15 +202,14 @@ def main():
                             Longitud = data_geoIP["longitude"]
                             ISP = "null"
                         else:
-                            ip_address = Country = City = regionName = ISP = Latitud = Longitud = "null"
+                            Country = City = regionName = ISP = Latitud = Longitud = "null"
                         date_Insert = time.strftime("%H:%M:%S")
                         date_Update = "none"
                         insert_mongodb(ip_address, Country, City, regionName, ISP, port, Banner, Latitud, Longitud, date_Insert, date_Update)
+                break
+            else:
+                print "[ERROR] Invalid target: " + target
 
-        else:
-            print "[ERROR] Invalid target: " + target
-
-
-if __name__ == '__main__':  
-    main() 
+if __name__ == '__main__':
+    main()
 
